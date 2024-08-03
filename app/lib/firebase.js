822ee4b9b29c1b579d58d34b36aa81eb36a756da
ehof17 <ehof17@gmail.com>
2024-08-03 14:35:20 -0700

    swag?

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -1,25 +1,28 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore,  collection, getDocs, query, where } from "firebase/firestore"; // Import Firestore
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-async function accessSecretVersion(name) {
-  const client = new SecretManagerServiceClient();   
+// const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+// async function accessSecretVersion(name) {
+//   const client = new SecretManagerServiceClient();   
 
-  const [version] = await client.accessSecretVersion({
-      name: name,
-  });
-  const payload = version.payload.data.toString('utf8');
-  return payload;
-}
-const secretName = 'projects/684375759874/secrets/NEXT_PUBLIC_FIREBASE_API_KEY';
-async function getFirebaseConfig() {
-  const apiKey = await accessSecretVersion(secretName);
-  console.log("We built this city.... Yeah We built ths city")
-  console.log(apiKey)
-  
-  return firebaseConfig;
-}
+//   const [version] = await client.accessSecretVersion({
+//       name: name,
+//   });
+//   const payload = version.payload.data.toString('utf8');
+//   return payload;
+// }
+// const secretName = 'projects/684375759874/secrets/NEXT_PUBLIC_FIREBASE_API_KEY';
+// async function getFirebaseConfig() {
+//   const apiKey = await accessSecretVersion(secretName);
+//   console.log("We built this city.... Yeah We built ths city")
+//   console.log(apiKey)
+
+//   return firebaseConfig;
+// }
+console.log("Did we build the city on rock and roll?")
+console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY)
 const firebaseConfig = {
+  
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
